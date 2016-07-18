4d38631e319f1dad80f0396bc249075bfaf6bff9
varesh360 <360logica>
2016-07-18 10:39:17 +0530

    test commit

---
+++ SeleniumGrid/src/com/tests/helloWorld.java
@@ -0,0 +1,45 @@
+package com.tests;
+ 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+ 
+public class helloWorld {
+   private WebDriver driver;
+   private String baseUrl;
+ 
+   @BeforeMethod
+   public void setUp() throws MalformedURLException {
+      URL hubUrl = new URL("http://loadtest.aliaswire.com:4445/wd/hub");
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName("firefox");
+      capabilities.setPlatform(Platform.WIN8_1);
+ 
+      driver = new RemoteWebDriver(hubUrl, capabilities);
+      baseUrl = "http://www.google.com/";
+      driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+   }
+ 
+   @Test
+   public void windowsTest() {
+      driver.get(baseUrl);
+      Reporter.log("Opened a browser");
+      
+      System.out.println("This is a Hello World Program");
+      
+   }
+ 
+   @AfterMethod
+   public void tearDown() {
+	   System.out.println("close driver");
+      driver.quit();
+   }
+}
